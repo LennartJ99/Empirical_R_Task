a443869b209b01bdbf79d1437ea2008297928148
LennartJ99 <lennart.joop@gmail.com>
2024-04-28 20:15:51 +0200

    :construction:

--- R/load/Load_bundestagwahl.R
+++ R/load/Load_bundestagwahl.R
@@ -1,5 +1,10 @@
 library(here)
-url<-"https://www.bundeswahlleiterin.de/dam/jcr/af56635e-73d7-404f-8745-9dbf2e023de3/btw2021kreis.csv"
-download.file(url, destfile=here("output","btw2021kreis.csv")
-  
-)
+url<-"https://www.bundeswahlleiterin.de/dam/jcr/0d1ea773-f3ca-40ea-b8ff-b031712707e1/btw17_kerg2.csv"
+download.file(url, destfile=here("input","btw2021kreis.csv"))
+
+Election2021<-read.csv(here("input","btw2021kreis.csv"), sep=";", header=TRUE, encoding="UTF-8",skip=9)
+
+url<-"https://www.bundeswahlleiterin.de/dam/jcr/860495c9-83fb-4068-8a99-c1c985ffffd2/w-btw21_kerg2.csv"
+download.file(url, destfile=here("input","btw17kreis.csv"))
+
+Election2017<-read.csv(here("input","btw17kreis.csv"), sep=";", header=TRUE, encoding="UTF-8",skip=9)
