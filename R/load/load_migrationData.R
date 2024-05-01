f4b5a3eeaa250b7025f9d486b824b36ee06fe4a7
LennartJ99 <lennart.joop@gmail.com>
2024-05-01 17:53:18 +0200

    improvements in data loading

--- R/load/load_migrationData.R
+++ R/load/load_migrationData.R
@@ -1,6 +1,13 @@
-library(wiesbaden)
-library(dotenv)
+library(here)
+library(dplyr)
 
-d <- retrieve_datalist(tableseries="125*", genesis=c(db="regio",user=Sys.getenv("unGenesis"), password=Sys.getenv("pwGenesis")))      
-data_migration<- retrieve_data(tablename="12521-0041", genesis=c(db="regio",user=Sys.getenv("unGenesis"), password=Sys.getenv("pwGenesis")))
+migration2022<-read.csv(here("input","migration_data2022.csv"), sep=";",skip=6, header=FALSE, encoding="UTF-8")
 
+migration2022<-migration2022%>%
+  rename(Year=V1,Number=V2,Name=V3,Origin=V4,Man=V5,Woman=V6,Total=V7)%>%
+  filter(Origin=="EU-27 (seit 01.02.2020)")%>%
+  mutate(Number = paste0("0", Number))
+  
+spatial_federal_districts<-spatial_federal_districts %>%
+  select(ARS, geometry)
+  
