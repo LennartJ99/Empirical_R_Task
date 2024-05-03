d0cbfc924630b314c07b5edd1a0c0a7d5cf6855a
LennartJ99 <lennart.joop@gmail.com>
2024-05-03 17:58:54 +0200

    Add migration data 2022

--- R/wrangle/Combined_Data.R
+++ R/wrangle/Combined_Data.R
@@ -1,10 +1,9 @@
-
 library(dplyr)
 library(sf)
+###Combine federal and voting districts
 Combined_Data <- st_intersection(spatial_federal_districts, spatial_voting_districts)
 
 Combined_Data$intersection_area <- st_area(Combined_Data)
-
 
 Combined_Data<-Combined_Data%>%
   group_by(ARS)%>%
@@ -20,3 +19,16 @@ group_by(ARS)%>%
   ungroup()
 
 Combined_Data$percentage<-as.numeric(as.character(Combined_Data$percentage))
+
+###Implement Migration Data
+Combined_Data<-left_join(Combined_Data,migration2022%>%select(Year,Man,Woman,Total,Number),by=c("ARS"="Number"))
+Combined_Data$Woman<-as.numeric(Combined_Data$Woman)
+Combined_Data$Man<-as.numeric(Combined_Data$Man)
+Combined_Data$Total<-as.numeric(Combined_Data$Total)
+
+Combined_Data<-Combined_Data%>%
+  mutate(Man2022=Man*percentage/100,
+         Woman2022=Woman*percentage/100,
+         Total2022=Total*percentage/100)
+
+###Implement Election Data
