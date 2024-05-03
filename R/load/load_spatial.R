a28992e0dfc2fea0b67ab8affaed47d52a193ecf
LennartJ99 <lennart.joop@gmail.com>
2024-05-03 17:59:07 +0200

    Implement spatial data

--- R/load/load_spatial.R
+++ R/load/load_spatial.R
@@ -3,11 +3,8 @@ library(here)
 spatial_voting_districts<-st_read(dsn = here("input","Geometrie_Wahlkreise_20DBT_VG250.shp"))
 spatial_federal_districts<-st_read(dsn = here("input","VG2500_KRS.shx"))
 
-library(ggplot2)
 
-
-ggplot() +
-  geom_sf(data = spatial_voting_districts)
-
-ggplot() +
-  geom_sf(data = spatial_federal_districts)
+spatial_federal_districts<-spatial_federal_districts %>%
+  select(ARS, geometry)
+spatial_voting_districts<-spatial_voting_districts %>%
+  select(WKR_NR, geometry)
