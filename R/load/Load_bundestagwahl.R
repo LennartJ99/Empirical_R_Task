3f28368d62d872efb3421a494c4647a1485f36ed
LennartJ99 <lennart.joop@gmail.com>
2024-06-06 16:57:39 +0200

    Filter relevant Data

--- R/load/Load_bundestagwahl.R
+++ R/load/Load_bundestagwahl.R
@@ -1,11 +1,11 @@
 library(here)
-url<-"https://www.bundeswahlleiterin.de/dam/jcr/0d1ea773-f3ca-40ea-b8ff-b031712707e1/btw17_kerg2.csv"
+url<-"https://www.bundeswahlleiterin.de/dam/jcr/860495c9-83fb-4068-8a99-c1c985ffffd2/w-btw21_kerg2.csv"
 download.file(url, destfile=here("input","btw2021kreis.csv"))
 
-Election2021<-read.csv(here("input","btw2021kreis.csv"), sep=";", header=TRUE, encoding="UTF-8",skip=9)
-
-url<-"https://www.bundeswahlleiterin.de/dam/jcr/860495c9-83fb-4068-8a99-c1c985ffffd2/w-btw21_kerg2.csv"
-download.file(url, destfile=here("input","btw17kreis.csv"))
-
-Election2017<-read.csv(here("input","btw17kreis.csv"), sep=";", header=TRUE, encoding="UTF-8",skip=9)
+Election<-read.csv(here("input","btw2021kreis.csv"), sep=";", header=TRUE, encoding="UTF-8",skip=9)
+Election<-Election%>%
+  filter(Gebietsart=="Wahlkreis")%>%
+  filter(Gruppenname=="AfD")%>%
+  filter(Stimme=="2")%>%
+  select(c("Gebietsnummer","Anzahl","Prozent","VorpAnzahl","VorpProzent"))
 
