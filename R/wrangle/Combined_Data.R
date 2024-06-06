1cbd9fd4433c99fe1172066b3c2060ade213c295
LennartJ99 <lennart.joop@gmail.com>
2024-06-06 16:58:43 +0200

    smooth Code, implement migration+election

--- R/wrangle/Combined_Data.R
+++ R/wrangle/Combined_Data.R
@@ -1,50 +1,80 @@
 library(dplyr)
 library(sf)
 library(tidyverse)
+library(rlang)
 ###Combine federal and voting districts
 Combined_Data17 <- st_intersection(spatial_federal_districts, spatial_voting_districts_17)
 Combined_Data <- st_intersection(spatial_federal_districts, spatial_voting_districts_21)
 
+###Calculate Intersection Areas. Due to some pairings appearing twice, we
+###add up the areas and union the geometries.
 Combined_Data17$intersection_area17<- st_area(Combined_Data17)
 Combined_Data17 <- Combined_Data17 %>%
   group_by(ARS, WKR_NR) %>%
   summarize(
     intersection_area17 = sum(intersection_area17),
     geometry = st_union(geometry)
-  )
+  )%>%
+  ungroup()
+
 Combined_Data$intersection_area21<- st_area(Combined_Data)
 Combined_Data <- Combined_Data %>%
   group_by(ARS, WKR_NR) %>%
   summarize(
     intersection_area21 = sum(intersection_area21),
-    geometry = st_union(geometry)
-  )
-Combined_Data17_df <- st_drop_geometry(Combined_Data17)
-
+    geometry = st_union(geometry) )%>%
+  ungroup()
+###dropping geometry, wont be needing it anymore.
+Combined_Data <- st_drop_geometry(Combined_Data)
+Combined_Data17 <- st_drop_geometry(Combined_Data17)
+Combined_Data17$intersection_area17<-as.numeric(Combined_Data17$intersection_area17)
+Combined_Data$intersection_area21<-as.numeric(Combined_Data$intersection_area21)
+###There were districts that overlapped in the spatial data but not in reality
+###By Trial and Error we found that 3% is a good threshold to filter out these districts
 Combined_Data<-Combined_Data%>%
-  left_join(.,Combined_Data17_df %>%
-              select(ARS, WKR_NR, intersection_area17), by = c("ARS", "WKR_NR"))
-
-
-
-Combined_Data<-Combined_Data%>%
-  group_by(ARS,WKR_NR)%>%
-  mutate(percentage17=intersection_area17/sum(intersection_area17)*100)%>%
+  group_by(ARS)%>%
   mutate(percentage21=intersection_area21/sum(intersection_area21)*100)%>%
   ungroup()
-
-Combined_Data$percentage17<-as.numeric(as.character(Combined_Data$percentage17))
 Combined_Data$percentage21<-as.numeric(as.character(Combined_Data$percentage21))
+Combined_Data<-Combined_Data%>%
+  group_by(ARS)%>%
+  filter(percentage21>3)%>%
+  mutate(percentage21=intersection_area21/sum(intersection_area21)*100)%>%
+  select(-c(intersection_area21))%>%
+  ungroup()
+
+
+Combined_Data17<-Combined_Data17%>%
+  group_by(ARS)%>%
+  mutate(percentage17=intersection_area17/sum(intersection_area17)*100)%>%
+  ungroup()
+Combined_Data17$percentage17<-as.numeric(as.character(Combined_Data17$percentage17))
+Combined_Data17<-Combined_Data17%>%
+  group_by(ARS)%>%
+  filter(percentage17>3)%>%
+  mutate(percentage17=intersection_area17/sum(intersection_area17)*100)%>%
+  select(-c(intersection_area17))%>%
+  ungroup()
+
+  
+
+###Before combining both datasets, we need to add "proxy" relations between federal
+###and voting districts, that didnt exist in 2017 but do in 2021.
+Combined_Data17<-Combined_Data17%>%
+add_row(ARS="09375",WKR_NR=234,percentage17=0)%>%
+add_row(ARS="09575",WKR_NR=242,percentage17=0)%>%
+add_row(ARS="09772",WKR_NR=254,percentage17=0)
 
 Combined_Data<-Combined_Data%>%
-  filter(percentage17>3)%>%
-  filter(percentage21>3)%>%
-  ###There were districts that overlapped in the spatial data but not in reality
-  ###By Trial and Error we found that 3% is a good threshold to filter out these districts
+  right_join(.,Combined_Data17 %>%
+              select(ARS, WKR_NR, percentage17), by = c("ARS", "WKR_NR"))
+
+
+
+
+
+Combined_Data<-Combined_Data%>%
 group_by(ARS)%>%
-  mutate(percentage17=intersection_area17/sum(intersection_area17)*100)%>%
-  mutate(percentage21=intersection_area21/sum(intersection_area21)*100)%>%
-  ungroup()%>%
   ###Now we manually adjust the percentage for big cities. We for example know, that Berlin is divided into
   ###12 districts, who have roughly the same population but vastly differ in size.
   ###Berlin
@@ -83,18 +113,86 @@ group_by(ARS)%>%
   mutate(percentage21=ifelse(WKR_NR>=217&WKR_NR<=220,25,percentage21))%>%
   ###Stuttgart
   mutate(percentage21=ifelse(WKR_NR==258|WKR_NR==259,50,percentage21))
-Combined_Data$percentage17<-as.numeric(as.character(Combined_Data$percentage17))
-Combined_Data$percentage21<-as.numeric(as.character(Combined_Data$percentage21))
-###Implement Migration Data
-Combined_Data<-left_join(Combined_Data,migration2022%>%select(Year,Man,Woman,Total,Number),by=c("ARS"="Number"))
-Combined_Data$Woman<-as.numeric(Combined_Data$Woman)
-Combined_Data$Man<-as.numeric(Combined_Data$Man)
-Combined_Data$Total<-as.numeric(Combined_Data$Total)
 
-Combined_Data<-Combined_Data %>%
-  mutate(Man2022=Man*percentage/100,
-         Woman2022=Woman*percentage/100,
-         Total2022=Total*percentage/100)%>%
-  select(-c(Man, Woman, Total))
+
+###Implement Migration Data
+
+Combined_Data_test<-Combined_Data
+Combined_Data<-Combined_Data_test  
+  Combined_Data <- Combined_Data %>%
+    left_join(migration2022 %>% select(Man, Woman, Total, Number), by = c("ARS" = "Number"))
+  Combined_Data<-Combined_Data%>%
+    rename(Man2022=`Man`)%>%
+    rename(Woman2022=`Woman`)%>%
+    rename(Total2022=`Total`)%>%
+    mutate(Man2022=as.numeric(Man2022)*percentage21/100,
+           Woman2022=as.numeric(Woman2022)*percentage21/100,
+           Total2022=as.numeric(Total2022)*percentage21/100)
+  
+  Combined_Data <- Combined_Data %>%
+    left_join(migration2021 %>% select(Man, Woman, Total, Number), by = c("ARS" = "Number"))
+  Combined_Data<-Combined_Data%>%
+    rename(Man2021=`Man`)%>%
+    rename(Woman2021=`Woman`)%>%
+    rename(Total2021=`Total`)%>%
+    mutate(Man2021=as.numeric(Man2021)*percentage21/100,
+           Woman2021=as.numeric(Woman2021)*percentage21/100,
+           Total2021=as.numeric(Total2021)*percentage21/100)
+
+  Combined_Data <- Combined_Data %>%
+    left_join(migration2020 %>% select(Man, Woman, Total, Number), by = c("ARS" = "Number"))
+  Combined_Data<-Combined_Data%>%
+    rename(Man2020=`Man`)%>%
+    rename(Woman2020=`Woman`)%>%
+    rename(Total2020=`Total`)%>%
+    mutate(Man2020=as.numeric(Man2020)*percentage21/100,
+           Woman2020=as.numeric(Woman2020)*percentage21/100,
+           Total2020=as.numeric(Total2020)*percentage21/100)%>%
+    mutate(Man2022=Man2022-Man2020,
+           Man2021=Man2021-Man2020,
+           Woman2022=Woman2022-Woman2020,
+           Woman2021=Woman2021-Woman2020,
+           Total2022=Total2022-Total2020,
+           Total2021=Total2021-Total2020)
+    
+
+  Combined_Data <- Combined_Data %>%
+    left_join(migration2018 %>% select(Man, Woman, Total, Number), by = c("ARS" = "Number"))
+  Combined_Data<-Combined_Data%>%
+    rename(Man2018=`Man`)%>%
+    rename(Woman2018=`Woman`)%>%
+    rename(Total2018=`Total`)%>%
+    mutate(Man2018=as.numeric(Man2018)*percentage17/100,
+           Woman2018=as.numeric(Woman2018)*percentage17/100,
+           Total2018=as.numeric(Total2018)*percentage17/100)
+
+  Combined_Data <- Combined_Data %>%
+    left_join(migration2017 %>% select(Man, Woman, Total, Number), by = c("ARS" = "Number"))
+  Combined_Data<-Combined_Data%>%
+    rename(Man2017=`Man`)%>%
+    rename(Woman2017=`Woman`)%>%
+    rename(Total2017=`Total`)%>%
+    mutate(Man2017=as.numeric(Man2017)*percentage17/100,
+           Woman2017=as.numeric(Woman2017)*percentage17/100,
+           Total2017=as.numeric(Total2017)*percentage17/100)
+
+  Combined_Data <- Combined_Data %>%
+    left_join(migration2016 %>% select(Man, Woman, Total, Number), by = c("ARS" = "Number"))
+  Combined_Data<-Combined_Data%>%
+    rename(Man2016=`Man`)%>%
+    rename(Woman2016=`Woman`)%>%
+    rename(Total2016=`Total`)%>%
+    mutate(Man2016=as.numeric(Man2016)*percentage17/100,
+           Woman2016=as.numeric(Woman2016)*percentage17/100,
+           Total2016=as.numeric(Total2016)*percentage17/100)%>%
+    mutate(Man2018=Man2018-Man2016,
+           Man2017=Man2017-Man2016,
+           Woman2018=Woman2018-Woman2016,
+           Woman2017=Woman2017-Woman2016,
+           Total2018=Total2018-Total2016,
+           Total2017=Total2017-Total2016)
+
 
 ###Implement Election Data
+  
+  
