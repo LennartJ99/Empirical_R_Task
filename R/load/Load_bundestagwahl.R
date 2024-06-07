f806e09be42dde8d9c3aa3582e149c1111356859
LennartJ99 <lennart.joop@gmail.com>
2024-06-07 15:33:23 +0200

    Fixed numeric Bug with Percentages

--- R/load/Load_bundestagwahl.R
+++ R/load/Load_bundestagwahl.R
@@ -2,11 +2,21 @@ library(here)
 url<-"https://www.bundeswahlleiterin.de/dam/jcr/860495c9-83fb-4068-8a99-c1c985ffffd2/w-btw21_kerg2.csv"
 download.file(url, destfile=here("input","btw2021kreis.csv"))
 
-Election<-read.csv(here("input","btw2021kreis.csv"), sep=";", header=TRUE, encoding="UTF-8",skip=9)
+Election<-read.csv(here("input","btw2021kre-is.csv"), sep=";", header=TRUE, encoding="UTF-8",skip=9)
 Election<-Election%>%
   filter(Gebietsart=="Wahlkreis")%>%
   filter(Gruppenname=="AfD")%>%
   filter(Stimme=="2")%>%
-  select(c("Gebietsnummer","Anzahl","Prozent","VorpAnzahl","VorpProzent"))%>%
-  mutate(Prozent=as.numeric(Prozent))
+  select(c("Gebietsnummer","Anzahl","Prozent","VorpAnzahl","VorpProzent"))
+###Percentages from the Datatset are in character, and i cant convert them to numeric because of the commas.
+###To solve this Problem we transform the comma to a point, comma and convert to numeric.
+extract_clean_convert <- function(x) {
+  cleaned <- gsub(",", ".", x)  # Remove commas
+  X <- as.numeric(cleaned)
+  # Convert to numeric
+  return(X)
+}
 
+# Apply the function to each element of the vector
+Election<-Election%>%
+  mutate(Prozent=as.numeric(sub(",", ".", Prozent, fixed=TRUE)))
